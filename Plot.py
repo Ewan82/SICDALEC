c0572c30fa3c6de8fe73b9a2026fb9ca662b13e3
Ewan Pinnington <ewan.pinnington@gmail.com>
2014-09-06 19:21:06 +0100

    just a few changes

--- Plot.py
+++ Plot.py
@@ -3,9 +3,9 @@ import SIC as SIC
 
 def PlotsuccSIC(d,i,j):
     Xlist=np.arange(i,j,1)
-    SIClist=[]
+    SIClist=[-9999]*(j-i)
     for x in range(i,j):
-         SIClist.append(SIC.SIC(d,i,x+1))
+         SIClist[x-i]=SIC.SIC(d,i,x+1)
 
     import matplotlib.pyplot as plt
 
@@ -15,17 +15,17 @@ def PlotsuccSIC(d,i,j):
     plt.title('SIC varying with successive observations of NEE')
     plt.show()
     
-def PlotoneSIC(i,j):
+def PlotoneSIC(d,i,j):
     Xlist=np.arange(i,j,1)
-    SIClist=[]
+    SIClist=[-9999]*(j-i)
     for x in range(i,j):
-        SIClist.append(SIC.SIC(x,x+1))
+        SIClist[x-i]=SIC.SIC(d,x,x+1)
         
     y=[sum(SIClist[:i+1]) for i in range(len(SIClist))]
     
     import matplotlib.pyplot as plt
 
-    plt.plot(Xlist,SIClist)
+    plt.plot(Xlist,y)
     plt.xlabel('Day of observation of NEE')
     plt.ylabel('Shannon Information Content')
     plt.title('SIC varying with successive observations of NEE')
