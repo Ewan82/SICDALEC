dcc733dc6526b2fbda155735acfb1e51cd1b0c44
Ewan Pinnington <ewan.pinnington@gmail.com>
2014-09-17 15:59:12 +0100

    can do multiple observations

--- SIC.py
+++ SIC.py
@@ -3,6 +3,7 @@ import numpy as np
 import Model as Mod
 import sys
 import ad
+import re
 
 
 def Mfac(Mlist,a): #Matrix factoral to find product of M matrices
@@ -21,6 +22,10 @@ def Hmat(d,i,j,obs): #Creates H^hat matrix for given obs, i,j and data d
     
     Clist,Mlist=Mod.Modlist(d,i,j)
     
+    Obslist=re.findall(r'[^,;\s]+', obs)
+    
+    Hhold=[-9999]*len(Obslist)
+    
     obsdict={'NEE': Mod.NEE, 'LF': Mod.LF, 'LW': Mod.LW, 'Cf': Mod.Cf, 'Cr': Mod.Cr, 'Cw': Mod.Cw, 'Cl': Mod.Cl, 'Cs': Mod.Cs}
 
     for x in range(i,j):
@@ -29,7 +34,9 @@ def Hmat(d,i,j,obs): #Creates H^hat matrix for given obs, i,j and data d
         Cw=ad.adnumber(Clist[x-i][2])
         Cl=ad.adnumber(Clist[x-i][3])
         Cs=ad.adnumber(Clist[x-i][4])
-        Hlist[x-i]=ad.jacobian(obsdict[obs](Cf,Cr,Cw,Cl,Cs,x,d),[Cf,Cr,Cw,Cl,Cs])
+        for y in range(0,len(Obslist)):
+            Hhold[y]=ad.jacobian(obsdict[Obslist[y]](Cf,Cr,Cw,Cl,Cs,x,d),[Cf,Cr,Cw,Cl,Cs])
+        Hlist[x-i]=np.vstack(Hhold)
 	
     stacklist=[Hlist[0]]+[-9999]*(j-i-1) #Creates H hat matrix
     for x in range(1,j-i):
@@ -39,16 +46,32 @@ def Hmat(d,i,j,obs): #Creates H^hat matrix for given obs, i,j and data d
     
     return np.matrix(Hmat)
     
+    
+def Rmat(d,i,j,obs):
 
+    stdO_dict={'NEE': d.sigO_nee, 'LF': d.sigO_lf, 'LW': d.sigO_lw, 'Cf': d.sigO_cf, 'Cr': d.sigO_cr, 'Cw': d.sigO_cw, 'Cl': d.sigO_cl, 'Cs': d.sigO_cs}
+
+    Obslist=re.findall(r'[^,;\s]+', obs)
+    
+    sigO=[-9999]*len(Obslist)
+
+    for y in range(0,len(Obslist)):
+        sigO[y]=stdO_dict[Obslist[y]]
+        
+    R=np.matrix(np.identity((j-i)*len(Obslist)))
+    sigR=sigO*(j-i)
+    for x in range(0,(j-i)*len(sigO)):
+        R[x,x]=sigR[x]
+            
+    return R
+      
+    
 def SIC(d,i,j,obs): #Calculates value of Shannon Info Content (SIC=0.5*ln(|B|/|A|), measure of reduction in entropy given a set of observations) 
     
-    stdO_dict={'NEE': d.sigO_nee, 'LF': d.sigO_lf, 'LW': d.sigO_lw, 'Cf': d.sigO_cf, 'Cr': d.sigO_cr, 'Cw': d.sigO_cw, 'Cl': d.sigO_cl, 'Cs': d.sigO_cs}
-    
     H=Hmat(d,i,j,obs)
-
-    sigO=stdO_dict[obs]
-    R=np.matrix(sigO*np.identity(j-i))	
-	
+    
+    R=Rmat(d,i,j,obs)
+    	
     B=d.B #Background error covariance matrix
 
     J2nddiff=B.I+H.T*R.I*H #Calculates Hessian
